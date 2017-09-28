6754986ed0a25c4c4d84e260650eb03722101c79
24duong@gmail.com <Haiduong24>
2017-09-28 15:49:34 +0700

    them animation

--- app/src/main/java/com/lhd/config/Config.java
+++ app/src/main/java/com/lhd/config/Config.java
@@ -1,6 +1,9 @@
 package com.lhd.config;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Vibrator;
 import android.util.Log;
 import android.widget.Toast;
@@ -25,46 +28,59 @@ public class Config {
         vibrator.cancel();
     }
 
-//    public static void run() {
+    //    public static void run() {
 //        isRunning = !isRunning;
 //        if (isRunning) {
 //
 //        }
 //    }
+    public static void goToStoreByPackageName(Context context) {
+        Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+//        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+//                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+//                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            context.startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            context.startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + context.getPackageName())));
+        }
+    }
 
-    public static void rungPosition(Context context, int positon) {
+    public static int rungPosition(Context context, int positon) {
         Toast.makeText(context, "" + positon, Toast.LENGTH_SHORT).show();
         if (isRunning) {
             switch (positon) {
                 case 1:
                     Log.e("leuleu", " 500, 1");
                     chayRung(context, 1000, 1);
-                    break;
+                    return 1;
                 case 2:
-                    Log.e("leuleu", " 500, 1");
-                    chayRung(context, 500, 1);
-                    break;
-                case 3:
                     Log.e("leuleu", "250, 250");
                     chayRung(context, 250, 250);
-                    break;
-                case 4:
+                    return 2;
+                case 3:
                     Log.e("leuleu", " 500, 250");
                     chayRung(context, 500, 250);
-                    break;
-                case 5:
+                    return 3;
+                case 4:
                     Log.e("leuleu", " 500, 500");
                     chayRung(context, 500, 500);
-                    break;
-                case 6:
+                    return 4;
+                case 5:
                     Log.e("leuleu", "1000, 500");
                     chayRung(context, 1000, 500);
-                    break;
+                    return 5;
             }
         } else {
             stopRung();
+            return 0;
         }
 
+        return positon;
     }
 
 
--- app/src/main/java/com/lhd/mvp/main/MainActivity.java
+++ app/src/main/java/com/lhd/mvp/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.lhd.mvp.main;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -11,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.lhd.config.Config;
 
 import butterknife.BindView;
@@ -28,18 +33,20 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout linearLayout;
     @BindView(R.id.relativeLayout)
     RelativeLayout relativeLayout;
+    @BindView(R.id.linearLayout2)
+    LinearLayout linearLayout2;
+    @BindView(R.id.button1)
+    Button button1;
     @BindView(R.id.button2)
     Button button2;
-    @BindView(R.id.button6)
-    Button button6;
-    @BindView(R.id.button)
-    Button button;
+    @BindView(R.id.button3)
+    Button button3;
     @BindView(R.id.button4)
     Button button4;
     @BindView(R.id.button5)
     Button button5;
-    @BindView(R.id.linearLayout2)
-    LinearLayout linearLayout2;
+    @BindView(R.id.im_bg_app)
+    ImageView imBgApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,38 +55,59 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         getSupportActionBar().hide();
 //        Config.chayRung(getBaseContext(), 1000, 1);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        Glide.with(this).load(R.drawable.bg_app).into(imBgApp);
     }
 
     public void onClick(View view) {
         Config.isRunning = true;
-        Config.rungPosition(this, Integer.parseInt((String) view.getTag()));
+        int positon = Config.rungPosition(this, Integer.parseInt((String) view.getTag()));
+        updateState(positon);
         if (Config.isRunning) {
-            txtState.setText(getResources().getText(R.string.start_massa));
-            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.vibration);
-            animation.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {
-
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-//                    expandableListViewAdapter.notifyDataSetChanged();
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {
-
-                }
-            });
+            txtState.setText(getResources().getText(R.string.stop_massa));
+            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_vibration_free);
+            switch (positon) {
+                case 1:
+                    Log.e("leuleu", " 500, 1");
+                    animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_vibration_free);
+                    break;
+                case 2:
+                    Log.e("leuleu", "250, 250");
+                    animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_vibration_250);
+                    break;
+                case 3:
+                    Log.e("leuleu", " 500, 250");
+                    animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_vibration_250);
+                    break;
+                case 4:
+                    Log.e("leuleu", " 500, 500");
+                    animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_vibration_500);
+                    break;
+                case 5:
+                    Log.e("leuleu", "1000, 500");
+                    animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_vibration_500);
+                    break;
+            }
             relativeLayout.startAnimation(animation);
-//            txtState.startAnimation(animation);
         } else {
             relativeLayout.clearAnimation();
             txtState.clearAnimation();
             txtState.setText(getResources().getText(R.string.stop_massa));
         }
+    }
 
+    private void updateState(int positon) {
+        Button[] btnItems = new Button[]{button1, button2, button3, button4, button5};
+        for (Button btn : btnItems)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                btn.setBackground(getResources().getDrawable(R.drawable.shape_btn_unselect));
+            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            btnItems[positon - 1].setBackground(getResources().getDrawable(R.drawable.shape_btn_select));
+        }
     }
 
     @OnClick(R.id.relativeLayout)
@@ -91,13 +119,19 @@ public class MainActivity extends AppCompatActivity {
             onClick(view);
         } else Config.stopRung();
         if (Config.isRunning)
-            txtState.setText(getResources().getText(R.string.start_massa));
+            txtState.setText(getResources().getText(R.string.stop_massa));
         else {
             relativeLayout.clearAnimation();
+            txtState.setText(getResources().getText(R.string.start_massa));
 //            txtState.clearAnimation();
-            txtState.setText(getResources().getText(R.string.stop_massa));
+
         }
 
 
+    }
+
+    @OnClick(R.id.im_store)
+    public void onViewClicked2() {
+        Config.goToStoreByPackageName(this);
     }
 }
